37c665df980973af9c714e2411841d54b0a53bda
yajaira <yajairaquipusco97@gmail.com>
2022-03-27 17:13:26 -0300

    regex de name y surname,validaciones de username y mail

--- src/app/pages/service/show-service/show-service-routing.module.ts
+++ src/app/pages/service/show-service/show-service-routing.module.ts
@@ -6,6 +6,6 @@ const routes: Routes = [{ path: '', component: ShowServiceComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ShowServiceRoutingModule { }
+export class ShowServiceRoutingModule {}
